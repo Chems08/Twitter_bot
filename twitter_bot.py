2e42147b0b16253456cc6a4cae95dce9282e9353
Chems <87978998+Chems08@users.noreply.github.com>
2022-08-26 00:25:51 +0200

    Update

--- twitter_bot.py
+++ twitter_bot.py
@@ -6,11 +6,13 @@ import tweepy
 list_des_valeurs_de_cours = []
 
 def valeur_de_cours():
-    symbol = 'AAPL'
+    symbol = 'AAPL' # Apple
     apikey = '...'
+    interval = '60min'
+    time_series = 'TIME_SERIES_INTRADAY'
     
 
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=',symbol,'&apikey=',apikey
+    url = 'https://www.alphavantage.co/query?function=',time_series,'&symbol=',symbol,'&interval=',interval,'&apikey=',apikey
     url = "".join(url)
 
     r = requests.get(url)
@@ -23,25 +25,30 @@ def valeur_de_cours():
 
     COURS = 0
 
+    time_interval = 'Time Series (60min)'       # 'Time Series (15min)' // 'Time Series (60min)' // 'Time Series (Daily)' 
+
     last_refresh = data["Meta Data"]["3. Last Refreshed"]
 
-    last_refresh = list(last_refresh)
+#_________________________________________________________________
+    # last_refresh = list(last_refresh)
 
 
-    if last_refresh[9] != " ":
-        last_refresh = last_refresh[:10]
+    # if last_refresh[9] != " ":
+    #     last_refresh = last_refresh[:10]
 
-    elif last_refresh[9] == " ":
-        last_refresh = last_refresh[:9]
+    # elif last_refresh[9] == " ":
+    #     last_refresh = last_refresh[:9]
+
+#__________________________________________________________________
 
     
     last_refresh = "".join(last_refresh)
 
     print("\n\nDate : ",last_refresh,"\n")
 
-    print("Le cours de l'action est de : $",data["Time Series (Daily)"][last_refresh]["1. open"])
+    print("Le cours de l'action est de : $",data[time_interval][last_refresh]["1. open"])
 
-    COURS = data["Time Series (Daily)"][last_refresh]["1. open"]
+    COURS = data[time_interval][last_refresh]["1. open"]
 
     list_des_valeurs_de_cours.append(COURS)
 
@@ -50,27 +57,27 @@ def valeur_de_cours():
 
     #select line of MIN cours value in the json file : 
 
-    MIN = 0
+    CLOSE = 0
     
-    print("Le minimum du cours de l'action est de : $",data["Time Series (Daily)"][last_refresh]["3. low"])
+    print("Le minimum du cours de l'action est de : $",data[time_interval][last_refresh]["4. close"])
 
-    MIN = data["Time Series (Daily)"][last_refresh]["3. low"]
+    CLOSE = data[time_interval][last_refresh]["4. close"]
 
-    list_des_valeurs_de_cours.append(MIN)
+    list_des_valeurs_de_cours.append(CLOSE)
 
     # 
 
 #___________________________________________________________________________
 
-    #select line of MAX cours value in the json file : 
+    #select line of MAX cours value in the json file : (INUTILE SUITE AU CHANGEMENT DU CODE)
 
-    MAX = 0
+    # MAX = 0
 
-    print("Le maximum du cours de l'action est de : $",data["Time Series (Daily)"][last_refresh]["2. high"],"\n")
+    # print("Le maximum du cours de l'action est de : $",data[time_interval][last_refresh]["2. high"],"\n")
 
-    MAX = data["Time Series (Daily)"][last_refresh]["2. high"]
+    # MAX = data[time_interval][last_refresh]["2. high"]
 
-    list_des_valeurs_de_cours.append(MAX)
+    # list_des_valeurs_de_cours.append(MAX)
 
     
     return 0
@@ -93,7 +100,13 @@ def tweet_avertir_min(compteur_en_baisse,prix_en_baisse):
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    status = "Le cours est en baisse depuis",compteur_en_baisse,"jours ! Il est actuellement à",prix_en_baisse,"dollars."
+    if compteur_en_baisse == 1:
+        heures = 'heure'
+    else:
+        heures = 'heures'
+
+    status = "Le cours est en baisse depuis",compteur_en_baisse,heures,"! - Il est actuellement à",prix_en_baisse,"US Dollars."
+    status = ' '.join(str(i) for i in status)
     api.update_status(status=status)
 
     return 0
@@ -111,7 +124,13 @@ def tweet_avertir_max(compteur_en_hausse,prix_en_hausse):
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    status = "Le cours est en baisse depuis",compteur_en_hausse,"jours ! Il est actuellement à",prix_en_hausse,"dollars."
+    if compteur_en_hausse == 1:
+        heures = 'heure'
+    else:
+        heures = 'heures'
+
+    status = "Le cours est en baisse depuis",compteur_en_hausse,heures,"! - Il est actuellement à",prix_en_hausse,"US Dollars."
+    status = ' '.join(str(i) for i in status)
     api.update_status(status=status)
 
     return 0
@@ -125,20 +144,32 @@ while True:
 
     valeur_de_cours()
 
-    if list_des_valeurs_de_cours[0] < list_des_valeurs_de_cours[2]:
+    if list_des_valeurs_de_cours[0] > list_des_valeurs_de_cours[1]:     # open > close
         compteur_en_hausse += 1
-        print("Analyse : \n")
-        print("Le cours est en hausse depuis",compteur_en_hausse,"jours ! \n")
+        
+        if compteur_en_hausse == 1:
+            heures = 'heure'
+        else:
+            heures = 'heures'
 
-        tweet_avertir_max(compteur_en_hausse,list_des_valeurs_de_cours[2])
+        print("Analyse : \n")
+        print("Le cours est en hausse depuis",compteur_en_hausse,heures,"! \n")
+
+        tweet_avertir_max(compteur_en_hausse,list_des_valeurs_de_cours[1])
 
         compteur_en_baisse = 0
         list_des_valeurs_de_cours = []
     
-    elif list_des_valeurs_de_cours[0] > list_des_valeurs_de_cours[1]:
+    elif list_des_valeurs_de_cours[0] < list_des_valeurs_de_cours[1]:    # open > close
         compteur_en_baisse += 1
+
+        if compteur_en_baisse == 1:
+            heures = 'heure'
+        else:
+            heures = 'heures'
+
         print("Analyse : \n")
-        print("Le cours est en baisse depuis",compteur_en_baisse,"jours ! \n")
+        print("Le cours est en baisse depuis",compteur_en_baisse,heures,"! \n")
 
         tweet_avertir_min(compteur_en_baisse,list_des_valeurs_de_cours[1])
 
@@ -148,6 +179,6 @@ while True:
     else :
         print("Le cours est resté stable ! ")
     
-    print("Mise à jour demain...\n")
+    print("Mise à jour dans une heure...\n")
     
-    time.sleep(86400) # = 1 jour
+    time.sleep(3600) # = 15min (900) / 1h (3600) / 1 jour (86400)
